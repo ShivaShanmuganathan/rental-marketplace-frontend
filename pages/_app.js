abefa19fc09a292383faf1c3cea9bd4e4227eeef
Shiva <shanshiva@outlook.com>
2022-02-21 00:34:48 +0530

    Class Error Resolved

--- pages/_app.js
+++ pages/_app.js
@@ -13,49 +13,49 @@ function Marketplace({ Component, pageProps }) {
         <div className="flex mt-4">
           
           <Link href="/">
-            <a class="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
-              <span class="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-              <span class="absolute inset-0 w-full h-full bg-white rounded-md "></span>
-              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
-              <span class="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Home</span>
+            <a className="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
+              <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+              <span className="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Home</span>
             </a>
           </Link>
           
           <Link href="/create-item">
-            <a class="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
-              <span class="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-              <span class="absolute inset-0 w-full h-full bg-white rounded-md "></span>
-              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
-              <span class="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Create NFTs</span>
+            <a className="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
+              <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+              <span className="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Create NFTs</span>
             </a>
           </Link>
           
 
 
           <Link href="/my-assets">
-            <a class="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
-              <span class="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-              <span class="absolute inset-0 w-full h-full bg-white rounded-md "></span>
-              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
-              <span class="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">My NFTs</span>
+            <a className="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
+              <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+              <span className="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">My NFTs</span>
             </a>
           </Link>
 
           <Link href="/my-rents">
-            <a class="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
-              <span class="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-              <span class="absolute inset-0 w-full h-full bg-white rounded-md "></span>
-              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
-              <span class="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Rented NFTs</span>
+            <a className="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
+              <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+              <span className="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Rented NFTs</span>
             </a>
           </Link>
 
           <Link href="/claimable-assets">
-            <a class="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
-              <span class="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
-              <span class="absolute inset-0 w-full h-full bg-white rounded-md "></span>
-              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
-              <span class="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Claimable NFTs</span>
+            <a className="relative inline-flex items-center justify-center px-6 py-3 text-black font-medium tracking-tighter text-white bg-gray-800 mr-6 rounded-md group">
+              <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-pink-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-pink-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+              <span className="relative text-2xl text-pink-600 font-bold transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">Claimable NFTs</span>
             </a>
           </Link>
 
